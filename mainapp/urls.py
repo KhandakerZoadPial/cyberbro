bfae53cd0da3fd35d9fceac5d7684a9c6dbdf7cb
KhandakerZoadPial <khpial27@gmail.com>
2024-01-06 16:49:43 +0600

    more changes on detail view

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
    path('', views.home, name='home'),
    path('research', views.research, name='research'),
-   path('about', views.about, name='about')
+   path('about', views.about, name='about'),
+   path('profile', views.profile, name='profile'),
+   path('all_vol', views.all_vol, name='all_vol'),
+   path('all_org', views.all_org, name='all_org'),
+   path('view_profile/<int:user_id>', views.view_profile, name='view_profile')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from accounts.models import *
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -11,3 +13,57 @@ def research(request):
 
 def about(request):
     return render(request, 'mainapp/about.html')
+
+
+def profile(request):
+    user = request.user
+    if user.is_authenticated:
+        context = {
+
+        }
+        print(Volunteer.objects.all())
+        
+        if Volunteer.objects.filter(user=user).count() > 0:
+            vol = Volunteer.objects.filter(user=user)[0]
+            context['user_type'] = 'Volunteer'
+            context['profile'] = vol
+        else:
+            org = Organization.objects.filter(user=user)[0]
+            context['user_type'] = 'Organization'
+            context['profile'] = org
+        
+        return render(request, 'mainapp/profile.html', context)
+    else:
+        return redirect('login')
+
+
+def all_vol(request):
+    context = {
+        'vol_set': Volunteer.objects.all()
+    }
+    return render(request, 'mainapp/all_vol.html', context)
+
+
+def all_org(request):
+    context = {
+        'org_set': Organization.objects.all()
+    }
+    return render(request, 'mainapp/all_org.html', context)
+
+
+def view_profile(request, user_id):
+    context = {
+
+    }
+    user = User.objects.get(pk=user_id)
+    if Volunteer.objects.filter(user=user).count() > 0:
+        vol = Volunteer.objects.filter(user=user)[0]
+        context['user_type'] = 'Volunteer'
+        context['profile'] = vol
+    else:
+        org = Organization.objects.filter(user=user)[0]
+        context['user_type'] = 'Organization'
+        context['profile'] = org
+    
+    return render(request, 'mainapp/profile.html', context)
+        
